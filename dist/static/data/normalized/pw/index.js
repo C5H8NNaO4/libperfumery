4d5ee333be91df7142171cb2581d58402796a587
Moritz Roessler <morrossl@publicisgroupe.net>
2025-03-05 18:22:16 +0100

    fix: remove fs dependency

--- dist/static/data/normalized/pw/index.js
+++ dist/static/data/normalized/pw/index.js
@@ -1,10 +1,10 @@
-import fs from "fs";
 async function importPages() {
     const pageNumbers = Array.from({ length: 42 }).map((e, i) => i + 1); // Add or remove numbers as needed
-    const importPromises = pageNumbers.map((num) => {
-        if (fs.existsSync(`./page${num}.json`))
-            return import(`./page${num}.json`, { assert: { type: "json" } });
-    }).filter(Boolean);
+    const importPromises = pageNumbers
+        .map((num) => {
+        return import(`./page${num}.json`, { assert: { type: "json" } });
+    })
+        .filter(Boolean);
     const pages = await Promise.all(importPromises);
     const normalizedPW = pages.map((module) => module?.default).flat();
     return normalizedPW;
--- src/static/data/normalized/pw/index.ts
+++ src/static/data/normalized/pw/index.ts
@@ -1,10 +1,10 @@
-import fs from "fs";
 async function importPages() {
   const pageNumbers = Array.from({ length: 42 }).map((e, i) => i + 1); // Add or remove numbers as needed
-  const importPromises = pageNumbers.map((num) => {
-    if (fs.existsSync(`./page${num}.json`))
+  const importPromises = pageNumbers
+    .map((num) => {
       return import(`./page${num}.json`, { assert: { type: "json" } });
-  }).filter(Boolean);
+    })
+    .filter(Boolean);
 
   const pages = await Promise.all(importPromises);
   const normalizedPW = pages.map((module) => (module as any)?.default).flat();
