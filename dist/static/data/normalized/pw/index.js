1d6ef2d65be4fff767fd461faf1d9a50d2e9bfc1
Moritz Roessler <morrossl@publicisgroupe.net>
2025-04-01 00:20:05 +0200

    fix: use static imports

--- dist/static/data/normalized/pw/index.js
+++ dist/static/data/normalized/pw/index.js
@@ -1,12 +1,67 @@
-async function importPages() {
-    const pageNumbers = Array.from({ length: 42 }).map((e, i) => i + 1); // Add or remove numbers as needed
-    const importPromises = pageNumbers
-        .map((num) => {
-        return import(`./page${num}.json`, { assert: { type: "json" } });
-    })
-        .filter(Boolean);
-    const pages = await Promise.all(importPromises);
-    const normalizedPW = pages.map((module) => module?.default).flat();
-    return normalizedPW;
-}
-export default importPages();
+import page1 from "./page1.json" assert { type: "json" };
+import page2 from "./page2.json" assert { type: "json" };
+import page3 from "./page3.json" assert { type: "json" };
+import page4 from "./page4.json" assert { type: "json" };
+import page5 from "./page5.json" assert { type: "json" };
+import page6 from "./page6.json" assert { type: "json" };
+import page7 from "./page7.json" assert { type: "json" };
+import page8 from "./page8.json" assert { type: "json" };
+import page9 from "./page9.json" assert { type: "json" };
+import page10 from "./page10.json" assert { type: "json" };
+import page21 from "./page21.json" assert { type: "json" };
+import page22 from "./page22.json" assert { type: "json" };
+import page23 from "./page23.json" assert { type: "json" };
+import page24 from "./page24.json" assert { type: "json" };
+import page25 from "./page25.json" assert { type: "json" };
+import page26 from "./page26.json" assert { type: "json" };
+import page27 from "./page27.json" assert { type: "json" };
+import page28 from "./page28.json" assert { type: "json" };
+import page29 from "./page29.json" assert { type: "json" };
+import page30 from "./page30.json" assert { type: "json" };
+import page31 from "./page31.json" assert { type: "json" };
+import page32 from "./page32.json" assert { type: "json" };
+import page33 from "./page33.json" assert { type: "json" };
+import page34 from "./page34.json" assert { type: "json" };
+import page35 from "./page35.json" assert { type: "json" };
+import page36 from "./page36.json" assert { type: "json" };
+import page37 from "./page37.json" assert { type: "json" };
+import page38 from "./page38.json" assert { type: "json" };
+import page39 from "./page39.json" assert { type: "json" };
+import page40 from "./page40.json" assert { type: "json" };
+import page41 from "./page41.json" assert { type: "json" };
+import page42 from "./page42.json" assert { type: "json" };
+const normalized = [
+    page1,
+    page2,
+    page3,
+    page4,
+    page5,
+    page6,
+    page7,
+    page8,
+    page9,
+    page10,
+    page21,
+    page22,
+    page23,
+    page24,
+    page25,
+    page26,
+    page27,
+    page28,
+    page29,
+    page30,
+    page31,
+    page32,
+    page33,
+    page34,
+    page35,
+    page36,
+    page37,
+    page38,
+    page39,
+    page40,
+    page41,
+    page42,
+];
+export default normalized;
--- src/static/data/normalized/pw/index.ts
+++ src/static/data/normalized/pw/index.ts
@@ -1,14 +1,79 @@
-async function importPages() {
-  const pageNumbers = Array.from({ length: 42 }).map((e, i) => i + 1); // Add or remove numbers as needed
-  const importPromises = pageNumbers
-    .map((num) => {
-      return import(`./page${num}.json`, { assert: { type: "json" } });
-    })
-    .filter(Boolean);
+import page1 from "./page1.json" assert { type: "json" };
+import page2 from "./page2.json" assert { type: "json" };
+import page3 from "./page3.json" assert { type: "json" };
+import page4 from "./page4.json" assert { type: "json" };
+import page5 from "./page5.json" assert { type: "json" };
+import page6 from "./page6.json" assert { type: "json" };
+import page7 from "./page7.json" assert { type: "json" };
+import page8 from "./page8.json" assert { type: "json" };
+import page9 from "./page9.json" assert { type: "json" };
+import page10 from "./page10.json" assert { type: "json" };
+import page11 from "./page11.json" assert { type: "json" };
+import page12 from "./page12.json" assert { type: "json" };
+import page13 from "./page13.json" assert { type: "json" };
+import page14 from "./page14.json" assert { type: "json" };
+import page15 from "./page15.json" assert { type: "json" };
+import page16 from "./page16.json" assert { type: "json" };
+import page17 from "./page17.json" assert { type: "json" };
+import page18 from "./page18.json" assert { type: "json" };
+import page19 from "./page19.json" assert { type: "json" };
+import page20 from "./page20.json" assert { type: "json" };
+import page21 from "./page21.json" assert { type: "json" };
+import page22 from "./page22.json" assert { type: "json" };
+import page23 from "./page23.json" assert { type: "json" };
+import page24 from "./page24.json" assert { type: "json" };
+import page25 from "./page25.json" assert { type: "json" };
+import page26 from "./page26.json" assert { type: "json" };
+import page27 from "./page27.json" assert { type: "json" };
+import page28 from "./page28.json" assert { type: "json" };
+import page29 from "./page29.json" assert { type: "json" };
+import page30 from "./page30.json" assert { type: "json" };
+import page31 from "./page31.json" assert { type: "json" };
+import page32 from "./page32.json" assert { type: "json" };
+import page33 from "./page33.json" assert { type: "json" };
+import page34 from "./page34.json" assert { type: "json" };
+import page35 from "./page35.json" assert { type: "json" };
+import page36 from "./page36.json" assert { type: "json" };
+import page37 from "./page37.json" assert { type: "json" };
+import page38 from "./page38.json" assert { type: "json" };
+import page39 from "./page39.json" assert { type: "json" };
+import page40 from "./page40.json" assert { type: "json" };
+import page41 from "./page41.json" assert { type: "json" };
+import page42 from "./page42.json" assert { type: "json" };
 
-  const pages = await Promise.all(importPromises);
-  const normalizedPW = pages.map((module) => (module as any)?.default).flat();
-  return normalizedPW;
-}
+const normalized = [
+  page1,
+  page2,
+  page3,
+  page4,
+  page5,
+  page6,
+  page7,
+  page8,
+  page9,
+  page10,
+  page21,
+  page22,
+  page23,
+  page24,
+  page25,
+  page26,
+  page27,
+  page28,
+  page29,
+  page30,
+  page31,
+  page32,
+  page33,
+  page34,
+  page35,
+  page36,
+  page37,
+  page38,
+  page39,
+  page40,
+  page41,
+  page42,
+];
 
-export default importPages();
+export default normalized;
